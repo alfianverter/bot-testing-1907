473bb939470794905385894fd0e4a0e843cb0eb3
Hazmi35 <32807631+Hazmi35@users.noreply.github.com>
2018-03-21 17:47:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -259,7 +259,11 @@ bot.on("message", function(message) {
         case "test":
         try {
                 get('https://aws.random.cat/meow').then(response => {
-                message.channel.send(":cat: | Here is your random cat.", {files: [{attachment: response.body.file, name: `cat.${response.body.file.split('.')[2]}`}]});
+                var embed = new Discord.RichEmbed()
+		.setTitle(`:cat: | Here is your random cat.`)
+		.setColor(`GREEN`)
+		.setImage(`{files: [{attachment: response.body.file, name: `cat.${response.body.file.split('.')[2]}`}]}`)
+                message.channel.send(embed);
             })
 		} catch (e) {
 			console.log(e);
